ba611a73fb781ea53ca2744f1dc8e316635659f6
Błażej Bartoszewski <blazejbart@gmail.com>
2023-12-15 22:36:42 +0100

    feat: hide btn load more

--- src/index.js
+++ src/index.js
@@ -1,3 +1,153 @@
+// import axios from 'axios';
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const searchForm = document.getElementById('search-form');
+//   const galleryContainer = document.getElementById('gallery');
+//   const loadMoreButton = document.querySelector('.load-more');
+//   let currentPage = 1;
+//   let currentSearchQuery = '';
+
+//   searchForm.addEventListener('submit', async event => {
+//     event.preventDefault();
+//     currentPage = 1;
+//     currentSearchQuery = event.target.searchQuery.value;
+
+//     try {
+//       const response = await axios.get('https://pixabay.com/api/', {
+//         params: {
+//           key: '41258332-bc5b81f30b9173b6d7f6fa8ea',
+//           q: currentSearchQuery,
+//           image_type: 'photo',
+//           orientation: 'horizontal',
+//           safesearch: true,
+//           per_page: 40,
+//           page: currentPage,
+//         },
+//       });
+
+//       const images = response.data.hits;
+//       displayResults(images);
+//       if (images.length === 0 || images.length < 40) {
+//         hideLoadMoreButton();
+//         showAlert("We're sorry, but you've reached the end of search results.");
+//       } else {
+//         showLoadMoreButton();
+//       }
+//     } catch (error) {
+//       console.error('Error during search:', error);
+//       showAlert('Error during search. Please try again later.');
+//       hideLoadMoreButton();
+//     }
+//   });
+
+//   loadMoreButton.addEventListener('click', async () => {
+//     currentPage += 1;
+
+//     try {
+//       const response = await axios.get('https://pixabay.com/api/', {
+//         params: {
+//           key: '41258332-bc5b81f30b9173b6d7f6fa8ea',
+//           q: currentSearchQuery,
+//           image_type: 'photo',
+//           orientation: 'horizontal',
+//           safesearch: true,
+//           per_page: 40,
+//           page: currentPage,
+//         },
+//       });
+
+//       const newImages = response.data.hits;
+//       appendResults(newImages);
+//       if (newImages.length === 0 || newImages.length < 40) {
+//         hideLoadMoreButton();
+//         showAlert("We're sorry, but you've reached the end of search results.");
+//       }
+//     } catch (error) {
+//       console.error('Error loading more images:', error);
+//       hideLoadMoreButton();
+//     }
+//   });
+
+//   function displayResults(images) {
+//     galleryContainer.innerHTML = '';
+
+//     if (images.length === 0) {
+//       hideLoadMoreButton();
+//       showAlert('No results found.');
+//       return;
+//     }
+
+//     images.forEach(image => {
+//       const imageElement = createImageElement(image);
+//       galleryContainer.appendChild(imageElement);
+//     });
+//   }
+
+//   function appendResults(newImages) {
+//     if (newImages.length === 0) {
+//       hideLoadMoreButton();
+//       showAlert("We're sorry, but you've reached the end of search results.");
+//       return;
+//     }
+
+//     newImages.forEach(image => {
+//       const imageElement = createImageElement(image);
+//       galleryContainer.appendChild(imageElement);
+//     });
+//   }
+
+//   function createImageElement(image) {
+//     const photoCard = document.createElement('div');
+//     photoCard.classList.add('photo-card');
+
+//     const imageElement = document.createElement('img');
+//     imageElement.src = image.previewURL;
+//     imageElement.alt = image.tags;
+//     imageElement.loading = 'lazy';
+
+//     const infoContainer = document.createElement('div');
+//     infoContainer.classList.add('info');
+
+//     const likes = document.createElement('p');
+//     likes.classList.add('info-item');
+//     likes.innerHTML = `<b>Likes:</b> ${image.likes || 0}`;
+
+//     const views = document.createElement('p');
+//     views.classList.add('info-item');
+//     views.innerHTML = `<b>Views:</b> ${image.views || 0}`;
+
+//     const comments = document.createElement('p');
+//     comments.classList.add('info-item');
+//     comments.innerHTML = `<b>Comments:</b> ${image.comments || 0}`;
+
+//     const downloads = document.createElement('p');
+//     downloads.classList.add('info-item');
+//     downloads.innerHTML = `<b>Downloads:</b> ${image.downloads || 0}`;
+
+//     infoContainer.appendChild(likes);
+//     infoContainer.appendChild(views);
+//     infoContainer.appendChild(comments);
+//     infoContainer.appendChild(downloads);
+
+//     photoCard.appendChild(imageElement);
+//     photoCard.appendChild(infoContainer);
+
+//     return photoCard;
+//   }
+
+//   function showLoadMoreButton() {
+//     loadMoreButton.style.display = 'block';
+//   }
+
+//   function hideLoadMoreButton() {
+//     loadMoreButton.style.display = 'none';
+//   }
+
+//   function showAlert(message) {
+//     alert(message);
+//   }
+// });
+
 import axios from 'axios';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -17,6 +167,9 @@ document.addEventListener('DOMContentLoaded', () => {
         params: {
           key: '41258332-bc5b81f30b9173b6d7f6fa8ea',
           q: currentSearchQuery,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: true,
           per_page: 40,
           page: currentPage,
         },
@@ -24,7 +177,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
       const images = response.data.hits;
       displayResults(images);
-      showLoadMoreButton();
+
+      if (images.length === 0 || images.length < 40) {
+        hideLoadMoreButton();
+        showAlert("We're sorry, but you've reached the end of search results.");
+      } else {
+        showLoadMoreButton();
+      }
     } catch (error) {
       console.error('Error during search:', error);
       showAlert('Error during search. Please try again later.');
@@ -40,6 +199,9 @@ document.addEventListener('DOMContentLoaded', () => {
         params: {
           key: '41258332-bc5b81f30b9173b6d7f6fa8ea',
           q: currentSearchQuery,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: true,
           per_page: 40,
           page: currentPage,
         },
@@ -47,6 +209,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
       const newImages = response.data.hits;
       appendResults(newImages);
+
+      if (newImages.length === 0 || newImages.length < 40) {
+        hideLoadMoreButton();
+        showAlert("We're sorry, but you've reached the end of search results.");
+      }
     } catch (error) {
       console.error('Error loading more images:', error);
       hideLoadMoreButton();
@@ -57,8 +224,8 @@ document.addEventListener('DOMContentLoaded', () => {
     galleryContainer.innerHTML = '';
 
     if (images.length === 0) {
-      showAlert('Error during search. Please try again later.');
       hideLoadMoreButton();
+      showAlert('No results found.');
       return;
     }
 
